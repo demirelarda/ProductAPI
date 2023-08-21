af1e51003b2d3ca86ee4a2df051a86378122d81f
Arda Demirel <yademirel21@gmail.com>
2023-08-21 03:08:44 +0300

    Added sorting system

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req,res) =>{ //FOR TESTING
 }
 
 const getAllProducts = async (req,res) =>{
-    const {featured, company, name} = req.query
+    const {featured, company, name, sort} = req.query
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true'? true : false
@@ -18,8 +18,16 @@ const getAllProducts = async (req,res) =>{
     if(name){
         queryObject.name = {$regex: name, $options: 'i'}
     }
-    console.log(queryObject)
-    const products = await Product.find(queryObject)
+
+    let result = Product.find(queryObject)
+    if(sort){
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
     res.status(200).json({products,nbHits:products.length})
 }
 
