83eb360c62402cb90ebf34b560ec00f8c63e0eef
Arda Demirel <yademirel21@gmail.com>
2023-08-21 15:48:51 +0300

    Added pagination

--- controllers/products.js
+++ controllers/products.js
@@ -31,6 +31,13 @@ const getAllProducts = async (req,res) =>{
         const fieldsList = fields.split(',').join(' ')
         result = result.select(fieldsList)
     }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10 //default limit is 10
+    const skip = Number(page-1) * limit
+
+    result = result.skip(skip).limit(limit)
+
     const products = await result
     res.status(200).json({products,nbHits:products.length})
 }
