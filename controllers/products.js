eeb0f985aa70f6bc623c58bf02e44d0abe3d5daf
Arda Demirel <yademirel21@gmail.com>
2023-08-21 15:17:12 +0300

    Added select option

--- controllers/products.js
+++ controllers/products.js
@@ -7,7 +7,7 @@ const getAllProductsStatic = async (req,res) =>{ //FOR TESTING
 }
 
 const getAllProducts = async (req,res) =>{
-    const {featured, company, name, sort} = req.query
+    const {featured, company, name, sort, fields} = req.query
     const queryObject = {}
     if(featured){
         queryObject.featured = featured === 'true'? true : false
@@ -26,6 +26,10 @@ const getAllProducts = async (req,res) =>{
     }
     else{
         result = result.sort('createdAt')
+    }
+    if(fields){
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
     }
     const products = await result
     res.status(200).json({products,nbHits:products.length})
